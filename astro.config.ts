9f6104cb9f9c5b642b33fade30c12601826f050b
Aiden Green <70248258+blaqat@users.noreply.github.com>
2025-09-08 20:20:47 -0400

    Changed 404 Page

---
+++ astro.config.ts
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import cloudflare from "@astrojs/cloudflare";
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte()],
+  output: "server",
+  adapter: cloudflare(),
+});
---
+++ src/components/Player.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let song;
+</script>
+
+<div>
+  <h3>{song.title} - {song.artist}</h3>
+  <audio controls src={song.url}></audio>
+</div>
