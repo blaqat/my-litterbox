21ece4e071c89f45152d94b60d7b202001c689a6
Aiden Green <70248258+blaqat@users.noreply.github.com>
2025-09-17 13:53:48 -0400

    Added Resume

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()],
   output: "server",
   adapter: cloudflare(),
   site: "https://blaqat.net",
